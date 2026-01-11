60669ff32cc02170ee26a325454788fc8b3c8069
Probal Ghosh <probalghoshofficial@gmail.com>
2026-01-12 02:43:13 +0530

    fix: update ResumeLink to the correct Google Drive URL

--- src/lib/Links.tsx
+++ src/lib/Links.tsx
@@ -1,5 +1,5 @@
 export const XLink =  "https://x.com/"
 export const LinkedInLink =  "https://www.linkedin.com/in/ghoshprobal/"
 export const GithUbLink =  "https://github.com/prodot-com"
-export const ResumeLink =  "https://drive.google.com/file/d/1YvitF_EVQFcKCqD3CtKQp-n3bJoMr5xc/view?usp=sharing"
+export const ResumeLink =  "https://drive.google.com/file/d/1PVsSG98at1FJ6FshUc5U59WoaEdptJfO/view?usp=sharing"
 export const MailLink = "https://mail.google.com/mail/?view=cm&fs=1&to=xprobal52@gmail.com"
