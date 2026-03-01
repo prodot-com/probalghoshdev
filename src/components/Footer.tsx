efeead38598d1b14340548da0182dd13b3f8a651
Probal Ghosh <probalghoshofficial@gmail.com>
2026-03-01 16:08:57 +0530

    feat: add Decoration component and integrate it into Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,27 @@
 import { Copyright } from "lucide-react"
+import Deco from "./icons/Decoration";
 
 export function Footer () {
 
     const date = new Date();
 
     return (
-        <div className="mt-7 pt-5 pl-4 md:pl-8 border-t dark:border-neutral-700 flex justify-between gap-1 w-full max-w-5xl min-h-42.5">
-            <div className="pl-2 flex items-start">
-                <p className="flex items-center gap-1">
-                    <Copyright className="w-5 sm:w-5"/> 
-                    <span className="instrument-serif text-[19px] sm:text-[19px]">{date.getFullYear()} Designed & Developed by <span className="text-[21px] instrument-serif-bold underline ">Probal.</span></span>
+        <div className="min-h-42.5 mb-10">
+            <div className="mt-7 mb-7 pt-5 pl-4 md:pl-8 border-t dark:border-neutral-700 flex justify-between gap-1 w-full max-w-5xl ">
+                <div className="pl-2 flex items-start">
+                    <p className="flex items-center gap-1">
+                        <Copyright className="w-5 sm:w-5"/> 
+                        <span className="instrument-serif text-[19px] sm:text-[19px]">{date.getFullYear()} Designed & Developed by <span className="text-[21px] instrument-serif-bold underline ">Probal.</span></span>
+                    </p>
+                </div>
+                <p className="instrument-serif text-[19px] sm:text-[19px] flex items-start pr-10">
+                    Asia/Kolkata
                 </p>
             </div>
-            <p className="instrument-serif text-[19px] sm:text-[19px] flex items-start pr-10">
-                Asia/Kolkata
-            </p>
+
+            <div className="text-neutral-300 dark:text-neutral-700 w-full flex justify-center">
+                <Deco/>
+            </div>
         </div>
     )
 }
